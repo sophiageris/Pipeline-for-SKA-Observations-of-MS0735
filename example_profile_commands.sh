0501ed3d73f7dc4e6a2dcf3f7c58c02cb67862b7
Sophia Geris <sophiageris@gmail.com>
2024-03-26 22:11:20 +1300

    added profile commands

---
+++ example_profile_commands.sh
@@ -0,0 +1,40 @@
+yes
+no
+get-geo
+0
+0
+0
+0
+512
+2
+get-tel
+d
+f
+ska_mid_133_5bii.tel
+t \
+get-dir
+.
+\
+read-sz
+yes
+p
+./model_signal_map.fits 
+S
+make-ap
+N
+make-poin-obs
+01 00 00.00
+-30 00 00.00
+-2
+2
+10
+yes
+0.00608
+2
+n
+y
+sim_ms0735_8hr.fits
+sim_ms0735_8hr
+J
+exit
+yes
